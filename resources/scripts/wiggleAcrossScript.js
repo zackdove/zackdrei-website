20d23b5c702c5f56f37ba514005fc665de6a1fe9
zackdove <zdove@live.co.uk>
2020-03-11 23:32:51 +0000

    adding scripts

---
+++ resources/scripts/wiggleAcrossScript.js
@@ -0,0 +1,25 @@
+// Get the id of the <path> element
+var curve = document.getElementById("curve");
+// Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
+// window.addEventListener("scroll", myFunction);
+function myFunction() {
+    var column = document.getElementById("column");
+    var curveRect = curve.getBoundingClientRect();
+    var columnRect = column.getBoundingClientRect();
+    var curveTopPercent = 1-(curveRect.top / columnRect.bottom);
+    // console.log(triangleTopPercent);
+    var start = 0.5;
+    var end = 0.9;
+    var scrollpercent = (curveTopPercent/(end-start))+(start/(start-end));
+    if (scrollpercent <= 0) {
+        scrollpercent = 0;
+    } else if (scrollpercent >= 1) {
+        scrollpercent = 1;
+    }
+    console.log(scrollpercent);
+    var draw = length * scrollpercent;
+    // Reverse the drawing (when scrolling upwards)
+    // triangle.style.strokeDashoffset = length - draw;
+    var textPath = document.getElementById("aboutCurveTextPath");
+    textPath.setAttribute("startOffset", (-draw * 4000) + 1200)
+}
